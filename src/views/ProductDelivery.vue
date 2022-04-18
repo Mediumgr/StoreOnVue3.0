7ad5a7d6d88260742ec0de684076b844c65b61a7
Руслан <Mediumgr@yandex.ru>
2022-04-18 03:04:40 +0300

    description component

---
+++ src/views/ProductDelivery.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flexText">
+    <p class="text">
+      Your order will be delivered FREE of charge to the selected retail store.
+    </p>
+    <p class="text">
+      DELIVERY is carried out FREE OF CHARGE across all Russia at the order from
+      1000 rub. Delivery will be made to your nearest post office.
+    </p>
+    <p class="text">
+      Delivery is carried out on orders in the amount of 299 rubles
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.flexText {
+  display: flex;
+  flex-direction: column;
+}
+
+.text {
+  padding: 10px 0;
+}
+</style>
--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -1,40 +1,69 @@
 <template>
-  <div class="product-details">
-    <a
-      ><img
-        class="product-item__img"
-        :src="require(`/src/assets/img/${product.img}`)"
-        alt="photo"
-    /></a>
-
-    <div class="bottom">
-      <a href="#" class="text__mango">{{ product.name }}</a>
-      <p class="price">
-        <span> &#36;{{ product.price }}</span>
-        <span>
-          <i
-            ref="star"
-            @click="changeRating(n)"
-            v-for="n in 5"
-            :key="n"
-            class="fas fa-star fa-sm"
-          ></i>
-        </span>
-      </p>
+  <div class="product-detail">
+    <div class="flex">
+      <div>
+        <div class="flex-box">
+          <img
+            class="product-img"
+            :src="require(`/src/assets/img/${product.img}`)"
+            alt="photo"
+          />
+        </div>
+        <div class="bottom">
+          <a href="#" class="text__mango">{{ product.name }}</a>
+          <p class="price">
+            <span> &#36;{{ product.price }}</span>
+            <span>
+              <i
+                ref="star"
+                @click="changeRating(n)"
+                v-for="n in 5"
+                :key="n"
+                class="fas fa-star fa-sm"
+              ></i>
+            </span>
+          </p>
+        </div>
+      </div>
+      <div class="block">
+        <button
+          v-for="tab in tabs"
+          :key="tab"
+          :class="['tabBtns', { active: currentTab === tab }]"
+          @click="currentTab = tab"
+        >
+          {{ tab }}
+        </button>
+        <component :is="currentComponent"></component>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import EventService from "@/services/EventService.js";
+import ProductDescription from "@/views/ProductDescription.vue";
+import ProductDelivery from "@/views/ProductDelivery.vue";
 
 export default {
   //mixins changeRating()
+  name: "ProductDetails",
+  components: {
+    ProductDescription,
+    ProductDelivery,
+  },
   data() {
     return {
       product: null,
       id: 3,
+      currentTab: "Description",
+      tabs: ["Description", "Delivery"],
     };
+  },
+  computed: {
+    currentComponent() {
+      return "product-" + this.currentTab.toLowerCase();
+    },
   },
   created() {
     EventService.getProduct(this.id)
@@ -48,4 +77,31 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.info {
+  float: left;
+}
+.flex {
+  display: flex;
+}
+.flex-box {
+  width: 400px;
+}
+
+.price {
+  .fa,
+  .fas {
+    font-weight: 100;
+    cursor: pointer;
+  }
+}
+
+.product-img {
+  width: 100%;
+  height: 432px;
+}
+
+.block {
+  padding-left: 20px;
+}
+</style>
