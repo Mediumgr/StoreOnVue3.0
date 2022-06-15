4281584bd1d0bf0975501aaeb8d3775157dfbc36
Руслан <Mediumgr@yandex.ru>
2022-06-15 15:17:24 +0300

    final

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 //baseURL: "https://my-json-server.typicode.com/Mediumgr/StoreOnVue3.0",
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://protected-meadow-21837.herokuapp.com/",
   withCredentials: false,
   headers: {
     Accept: "application/json",
